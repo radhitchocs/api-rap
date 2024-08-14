99f194a3b80cf68ceed4109441c9909a6a799f6b
radhitchocs <radhitchocs@gmail.com>
2024-08-14 21:10:33 +0700

    fix point settings module

--- src/module/point-settings/controller/point-settings.controller.ts
+++ src/module/point-settings/controller/point-settings.controller.ts
@@ -1,20 +1,60 @@
-// src/module/point-settings/controller/point-settings.controller.ts
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import { ResponseMessage } from 'src/decorator/response.decorator';
 import { PointSettingsService } from '../service/point-settings.service';
 import { CreatePointSettingsDto } from '../dto/create-point-settings.dto';
+import { Types } from 'mongoose';
+import { PointSettingsEntity } from '../schema/point-settings.schema';
 
 @Controller('point-settings')
 export class PointSettingsController {
   constructor(private readonly pointSettingsService: PointSettingsService) {}
 
-  @Post()
-  async create(@Body() dto: CreatePointSettingsDto) {
-    return this.pointSettingsService.create(dto);
+  @Get()
+  @ResponseMessage('Successfully retrieved point settings!')
+  async get(@Query() dto: PointSettingsEntity) {
+    const result = await this.pointSettingsService.get(dto);
+    return result;
   }
 
-  @Get()
-  async get(@Query('id') id: string) {
-    return this.pointSettingsService.findOne(id);
+  @Get('/:id')
+  @ResponseMessage('Successfully retrieved details of point setting!')
+  async getById(@Param('id') id: string) {
+    const result = await this.pointSettingsService.getById(
+      new Types.ObjectId(id),
+    );
+    return result;
+  }
+
+  @Post()
+  @ResponseMessage('Point setting has been created successfully.')
+  async create(@Body() dto: CreatePointSettingsDto) {
+    const result = await this.pointSettingsService.create(dto);
+    return result;
+  }
+
+  @Patch('/:id')
+  @ResponseMessage('Point setting has been updated successfully.')
+  async update(@Param('id') id: string, @Body() dto: CreatePointSettingsDto) {
+    const updateData = {
+      ...dto,
+    };
+
+    return this.pointSettingsService.update(new Types.ObjectId(id), updateData);
+  }
+
+  @Delete('/:id')
+  @ResponseMessage('Point setting has been deleted successfully.')
+  async delete(@Param('id') id: string) {
+    return this.pointSettingsService.delete(new Types.ObjectId(id));
   }
 }
--- src/module/point-settings/service/point-settings.service.ts
+++ src/module/point-settings/service/point-settings.service.ts
@@ -1,24 +1,67 @@
-// src/module/point-settings/service/point-settings.service.ts
-
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { PointSettingsEntity } from '../schema/point-settings.schema';
+import { PaginateModel, PaginateResult, Types } from 'mongoose';
 import { CreatePointSettingsDto } from '../dto/create-point-settings.dto';
+import { PointSettingsEntity } from '../schema/point-settings.schema';
+import { PointSettingsInterface } from '../interface/point-settings.interface';
 
 @Injectable()
 export class PointSettingsService {
   constructor(
     @InjectModel(PointSettingsEntity.name)
-    private readonly pointSettingsModel: Model<PointSettingsEntity>,
+    private pointSettingsModel: PaginateModel<PointSettingsEntity>,
   ) {}
 
-  async create(dto: CreatePointSettingsDto): Promise<PointSettingsEntity> {
+  async get(dto: any): Promise<PaginateResult<PointSettingsEntity>> {
+    const { page, limit } = dto;
+    const options = {
+      page,
+      limit: limit || 10,
+      sort: {
+        createdAt: -1,
+      },
+    };
+
+    return this.pointSettingsModel.paginate({}, options);
+  }
+
+  async getById(id: Types.ObjectId): Promise<PointSettingsInterface> {
+    return this.pointSettingsModel.findById(id).exec();
+  }
+
+  async create(dto: CreatePointSettingsDto): Promise<PointSettingsInterface> {
     const newPointSettings = new this.pointSettingsModel(dto);
     return newPointSettings.save();
   }
 
-  async findOne(id: string): Promise<PointSettingsEntity> {
-    return this.pointSettingsModel.findById(id).exec();
+  async update(
+    id: Types.ObjectId,
+    dto: CreatePointSettingsDto,
+  ): Promise<PointSettingsInterface> {
+    const pointSetting = await this.pointSettingsModel.findById(id).lean();
+
+    if (!pointSetting) {
+      throw new Error('Point setting not found');
+    }
+    return await this.pointSettingsModel.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: dto,
+      },
+      { new: true },
+    );
+  }
+
+  async delete(id: Types.ObjectId) {
+    return await this.pointSettingsModel.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          deleted: true,
+          deletedAt: new Date(),
+        },
+      },
+      { new: true },
+    );
   }
 }
