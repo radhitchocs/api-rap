e201578bba8753b68322fc3372afd61238d8f3dc
radhitchocs <radhitchocs@gmail.com>
2024-08-14 20:37:33 +0700

    fix order point details module

--- src/module/order_points_details/controller/order-point-details.controller.ts
+++ src/module/order_points_details/controller/order-point-details.controller.ts
@@ -1,15 +1,41 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 import { ResponseMessage } from 'src/decorator/response.decorator';
 import { OrderPointDetailsService } from '../service/order-point-details.service';
 import { CreateOrderPointDetailDto } from '../dto/create-order-point-detail.dto';
 import { GetOrderPointDetailDto } from '../dto/get-order-point-detail.dto';
+import { Types } from 'mongoose';
 
 @Controller('order-point-details')
 export class OrderPointDetailsController {
   constructor(
     private readonly orderPointDetailsService: OrderPointDetailsService,
   ) {}
+
+  @Get()
+  @ResponseMessage('Successfully retrieved order point details!')
+  async get(@Query() dto: GetOrderPointDetailDto) {
+    const result = await this.orderPointDetailsService.get(dto);
+    return result;
+  }
+
+  @Get('/:id')
+  @ResponseMessage('Successfully retrieved order point detail!')
+  async getById(@Param('id') id: string) {
+    const result = await this.orderPointDetailsService.getById(
+      new Types.ObjectId(id),
+    );
+    return result;
+  }
 
   @Post()
   @ResponseMessage('Order point detail has been created successfully.')
@@ -18,10 +44,27 @@ export class OrderPointDetailsController {
     return result;
   }
 
-  @Get()
-  @ResponseMessage('Successfully retrieved order point details!')
-  async get(@Query() dto: GetOrderPointDetailDto) {
-    const result = await this.orderPointDetailsService.get(dto);
-    return result;
+  @Patch('/:id')
+  @ResponseMessage('Order point detail has been updated successfully.')
+  async update(
+    @Param('id') id: string,
+    @Body() dto: CreateOrderPointDetailDto,
+  ) {
+    const updateData = { ...dto };
+
+    if (!id) {
+      throw new Error('Order point detail ID is required');
+    }
+
+    return this.orderPointDetailsService.update(
+      new Types.ObjectId(id),
+      updateData,
+    );
+  }
+
+  @Delete('/:id')
+  @ResponseMessage('Order point detail has been deleted successfully.')
+  async delete(@Param('id') id: string) {
+    return this.orderPointDetailsService.delete(new Types.ObjectId(id));
   }
 }
--- src/module/order_points_details/service/order-point-details.service.ts
+++ src/module/order_points_details/service/order-point-details.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { PaginateModel, PaginateResult } from 'mongoose';
+import { PaginateModel, PaginateResult, Types } from 'mongoose';
 import { CreateOrderPointDetailDto } from '../dto/create-order-point-detail.dto';
 import { GetOrderPointDetailDto } from '../dto/get-order-point-detail.dto';
 import { OrderPointDetailEntity } from '../schema/order-point-detail.schema';
@@ -11,13 +11,6 @@ export class OrderPointDetailsService {
     @InjectModel(OrderPointDetailEntity.name)
     private orderPointDetailModel: PaginateModel<OrderPointDetailEntity>,
   ) {}
-
-  async create(
-    dto: CreateOrderPointDetailDto,
-  ): Promise<OrderPointDetailEntity> {
-    const newOrderPointDetail = new this.orderPointDetailModel(dto);
-    return newOrderPointDetail.save();
-  }
 
   async get(
     dto: GetOrderPointDetailDto,
@@ -30,5 +23,50 @@ export class OrderPointDetailsService {
     if (points_earned) query.points_earned = points_earned;
 
     return this.orderPointDetailModel.paginate(query, { page: 1, limit: 10 });
+  }
+
+  async getById(id: Types.ObjectId): Promise<OrderPointDetailEntity> {
+    return this.orderPointDetailModel.findById(id).exec();
+  }
+
+  async create(
+    dto: CreateOrderPointDetailDto,
+  ): Promise<OrderPointDetailEntity> {
+    const newOrderPointDetail = new this.orderPointDetailModel(dto);
+    return newOrderPointDetail.save();
+  }
+
+  async update(
+    id: Types.ObjectId,
+    dto: CreateOrderPointDetailDto,
+  ): Promise<OrderPointDetailEntity> {
+    const orderPointDetail = await this.orderPointDetailModel
+      .findById(id)
+      .lean();
+
+    if (!orderPointDetail) {
+      throw new Error('Order point detail not found');
+    }
+
+    return await this.orderPointDetailModel.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: dto,
+      },
+      { new: true },
+    );
+  }
+
+  async delete(id: Types.ObjectId): Promise<OrderPointDetailEntity> {
+    return await this.orderPointDetailModel.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          deleted: true,
+          deletedAt: new Date(),
+        },
+      },
+      { new: true },
+    );
   }
 }
