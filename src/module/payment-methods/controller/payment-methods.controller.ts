218579a52cf0171144929a6f60fcb196d2445d10
radhitchocs <radhitchocs@gmail.com>
2024-08-14 20:59:53 +0700

    fix payment methods module

--- src/module/payment-methods/controller/payment-methods.controller.ts
+++ src/module/payment-methods/controller/payment-methods.controller.ts
@@ -1,19 +1,22 @@
-import { Body, Controller, Get, Post, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ResponseMessage } from 'src/decorator/response.decorator';
 import { PaymentMethodsService } from '../service/payment-methods.service';
 import { CreatePaymentMethodDto } from '../dto/create-payment-method.dto';
 import { GetPaymentMethodDto } from '../dto/get-payment-method.dto';
+import { Types } from 'mongoose';
 
 @Controller('payment-methods')
 export class PaymentMethodsController {
   constructor(private readonly paymentMethodsService: PaymentMethodsService) {}
-
-  @Post()
-  @ResponseMessage('Payment method has been created successfully.')
-  async create(@Body() dto: CreatePaymentMethodDto) {
-    const result = await this.paymentMethodsService.create(dto);
-    return result;
-  }
 
   @Get()
   @ResponseMessage('Successfully retrieved payment methods!')
@@ -27,5 +30,29 @@ export class PaymentMethodsController {
   async getById(@Param('id') id: string) {
     const result = await this.paymentMethodsService.getById(id);
     return result;
+  }
+
+  @Post()
+  @ResponseMessage('Payment method has been created successfully.')
+  async create(@Body() dto: CreatePaymentMethodDto) {
+    const result = await this.paymentMethodsService.create(dto);
+    return result;
+  }
+
+  @Patch('/:id')
+  @ResponseMessage('Payment method has been updated successfully.')
+  async update(@Param('id') id: string, @Body() dto: CreatePaymentMethodDto) {
+    const updateData = { ...dto };
+
+    return this.paymentMethodsService.update(
+      new Types.ObjectId(id),
+      updateData,
+    );
+  }
+
+  @Delete('/:id')
+  @ResponseMessage('Payment method has been deleted successfully.')
+  async delete(@Param('id') id: string) {
+    return this.paymentMethodsService.delete(new Types.ObjectId(id));
   }
 }
--- src/module/payment-methods/service/payment-methods.service.ts
+++ src/module/payment-methods/service/payment-methods.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { PaymentMethodEntity } from '../schema/payment-method.schema';
 import { CreatePaymentMethodDto } from '../dto/create-payment-method.dto';
 import { GetPaymentMethodDto } from '../dto/get-payment-method.dto';
@@ -12,18 +12,50 @@ export class PaymentMethodsService {
     private paymentMethodModel: Model<PaymentMethodEntity>,
   ) {}
 
-  async create(dto: CreatePaymentMethodDto): Promise<PaymentMethodEntity> {
-    const newPaymentMethod = new this.paymentMethodModel(dto);
-    return newPaymentMethod.save();
-  }
-
   async get(dto: GetPaymentMethodDto) {
     const { search } = dto;
     const query = search ? { name: new RegExp(search, 'i') } : {};
     return this.paymentMethodModel.find(query).exec();
   }
 
+  async create(dto: CreatePaymentMethodDto): Promise<PaymentMethodEntity> {
+    const newPaymentMethod = new this.paymentMethodModel(dto);
+    return newPaymentMethod.save();
+  }
+
   async getById(id: string): Promise<PaymentMethodEntity> {
     return this.paymentMethodModel.findById(id).exec();
+  }
+
+  async update(
+    id: Types.ObjectId,
+    dto: CreatePaymentMethodDto,
+  ): Promise<PaymentMethodEntity> {
+    const paymentMethod = await this.paymentMethodModel.findById(id).lean();
+
+    if (!paymentMethod) {
+      throw new Error('Payment method not found');
+    }
+
+    return await this.paymentMethodModel.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: dto,
+      },
+      { new: true },
+    );
+  }
+
+  async delete(id: Types.ObjectId) {
+    return await this.paymentMethodModel.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          deleted: true,
+          deletedAt: new Date(),
+        },
+      },
+      { new: true },
+    );
   }
 }
