5ca9abd0f5c44dcfb69770790bb8eca7e8ccdf12
radhitchocs <radhitchocs@gmail.com>
2024-08-15 19:36:06 +0700

    fix order-details module

--- src/module/order_details/order-details.module.ts
+++ src/module/order_details/order-details.module.ts
@@ -6,12 +6,16 @@ import {
 } from './schema/order-detail.schema';
 import { OrderDetailsController } from './controller/order_details.controller';
 import { OrderDetailsService } from './service/order-details.service';
+import { OrdersModule } from '../orders/orders.module';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: OrderDetailEntity.name, schema: OrderDetailSchema },
     ]),
+    OrdersModule,
+    ProductsModule,
   ],
   controllers: [OrderDetailsController],
   providers: [OrderDetailsService],
--- src/module/order_details/service/order-details.service.ts
+++ src/module/order_details/service/order-details.service.ts
@@ -1,19 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginateModel, Types } from 'mongoose';
 import { OrderDetailEntity } from '../schema/order-detail.schema';
 import { CreateOrderDetailDto } from '../dto/create-order-detail.dto';
 import { GetOrderDetailDto } from '../dto/get-order-detail.dto';
+import { OrdersService } from 'src/module/orders/service/orders.service';
+import { ProductsService } from 'src/module/products/service/products.service';
 
 @Injectable()
 export class OrderDetailsService {
   constructor(
     @InjectModel(OrderDetailEntity.name)
     private readonly orderDetailModel: PaginateModel<OrderDetailEntity>,
+    private readonly ordersService: OrdersService,
+    private readonly productsService: ProductsService,
   ) {}
 
   async create(dto: CreateOrderDetailDto): Promise<OrderDetailEntity> {
-    const newOrderDetail = new this.orderDetailModel(dto);
+    // Ambil orderId dari koleksi orders
+    const order = await this.ordersService.getById(
+      new Types.ObjectId(dto.orderId),
+    );
+    if (!order) {
+      throw new NotFoundException(`Order with ID "${dto.orderId}" not found`);
+    }
+
+    // Ambil productId dari koleksi products
+    const product = await this.productsService.getById(
+      new Types.ObjectId(dto.productId),
+    );
+    if (!product) {
+      throw new NotFoundException(
+        `Product with ID "${dto.productId}" not found`,
+      );
+    }
+
+    const newOrderDetail = new this.orderDetailModel({
+      ...dto,
+      orderId: new Types.ObjectId(dto.orderId), // Assign orderId as ObjectId
+      productId: new Types.ObjectId(dto.productId), // Assign productId as ObjectId
+    });
+
     return newOrderDetail.save();
   }
 
@@ -31,20 +58,37 @@ export class OrderDetailsService {
   }
 
   async update(
-    orderId: Types.ObjectId,
+    id: Types.ObjectId,
     dto: CreateOrderDetailDto,
   ): Promise<OrderDetailEntity> {
-    const order = await this.orderDetailModel.findById(orderId).lean();
-    if (!order) {
-      throw new Error('Order not found');
+    const orderDetail = await this.orderDetailModel.findById(id).exec();
+    if (!orderDetail) {
+      throw new NotFoundException(`OrderDetail with ID "${id}" not found`);
     }
-    return await this.orderDetailModel.findOneAndUpdate(
-      { _id: orderId },
-      {
-        $set: dto,
-      },
-      { new: true },
-    );
+
+    if (dto.orderId) {
+      const order = await this.ordersService.getById(
+        new Types.ObjectId(dto.orderId),
+      );
+      if (!order) {
+        throw new NotFoundException(`Order with ID "${dto.orderId}" not found`);
+      }
+    }
+
+    if (dto.productId) {
+      const product = await this.productsService.getById(
+        new Types.ObjectId(dto.productId),
+      );
+      if (!product) {
+        throw new NotFoundException(
+          `Product with ID "${dto.productId}" not found`,
+        );
+      }
+    }
+
+    return await this.orderDetailModel
+      .findByIdAndUpdate(id, { $set: dto }, { new: true })
+      .exec();
   }
 
   async delete(orderId: Types.ObjectId): Promise<OrderDetailEntity> {
